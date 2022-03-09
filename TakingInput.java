99a48ecddd32065195a35ba95a7831a69688410c
RakeshKumarParida <rakeshkumarparida424@gmail.com>
2022-03-10 00:07:17 +0530

    UserInput

---
+++ TakingInput.java
@@ -0,0 +1,34 @@
+//How to take input from users ?
+
+import java.util.Scanner;
+
+
+
+public class TakingInput {
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int num1 = scan1.nextInt();
+		
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("Enter the second Number");
+		int num2 = scan2.nextInt();
+		
+		
+		int num3 = num1+num2;
+		
+		System.out.println("The sum of two numbers is "+num3);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
